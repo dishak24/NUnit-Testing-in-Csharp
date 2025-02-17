c6a663be1d7d435591ee8cc92d54eb1b2000068f
Disha <dishakamble24@gmail.com>
2025-02-17 14:37:38 +0530

    Sorting code For test

---
+++ TestingProject/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingProject
+{
+    public class BubbleSort
+    {
+        public int[] SortArray(int[] array)
+        {
+            for ( int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+}
