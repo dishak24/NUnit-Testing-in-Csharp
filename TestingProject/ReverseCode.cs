5ddf956d9334b5f4abd28a9cf80e865e4c423332
Disha <dishakamble24@gmail.com>
2025-02-17 14:38:26 +0530

    Reverse String code testing function

---
+++ TestingProject/ReverseCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingProject
+{
+    public class ReverseCode
+    {
+        public string ReverseString(string name)
+        {
+            char[] arr = name.ToCharArray();
+            string result = "";
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                result += arr[i];
+            }
+
+            return result;
+        }
+    }
+}
