705d5a017eb775438df7584ffddce6a8dbe87d35
Disha <dishakamble24@gmail.com>
2025-02-17 14:37:01 +0530

    Addition Code

---
+++ TestingProject/SimpleCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingProject
+{
+    public class SimpleCode
+    {
+        public int AddNumbers(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+    }
+}
